e94096125c5f9e8a3519fd169b155b0b99c5c62a
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-04-10 20:06:09 +0700

    Adjust default layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,8 +1,6 @@
 <template>
-  <div
-    class="flex flex-col min-h-screen justify-between bg-white dark:bg-gray-800 text-gray-800 dark:text-white"
-  >
-    <header class="py-6 bg-gray-100 dark:bg-gray-700">
+  <div class="flex flex-col min-h-screen justify-between bg-white dark:bg-gray-800 text-gray-800 dark:text-gray-400">
+    <header class="py-6 bg-gray-300 dark:bg-gray-700 dark:text-white">
       <div class="container">
         <div class="flex justify-between items-center">
           <strong>
@@ -19,9 +17,17 @@
 
     <slot/>
 
-    <footer class="py-6 bg-gray-100 dark:bg-gray-700">
-      <div class="container text-center">
-        <p>&copy; 2022 {{ $static.metadata.siteName }}</p>
+    <footer class="container">
+      <hr class="dark:border-gray-400" />
+      <div class="py-6 space-y-2 lg:space-y-0 lg:flex lg:justify-between lg:items-center">
+        <p>Copyright &copy; 2022 {{ $static.metadata.siteName }}</p>
+        <nav class="flex items-center space-x-2">
+          <a href="#wip">FAQ</a>
+          <span>
+            |
+          </span>
+          <a href="#wip">Terms</a>
+        </nav>
       </div>
     </footer>
   </div>
