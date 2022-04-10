626ac10e4ef25388db50ba3f092d8ddd83fc5f4b
Budiman Fajar Firdaus <budimanfajarf@gmail.com>
2022-04-10 16:44:56 +0700

    Add Tailwind CSS & the config
    - Add tailwindcss: npm:@tailwindcss/postcss7-compat
    - Add postcss@^7
    - Add @tailwindcss/typography

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+// Import global styles. Including Tailwind CSS in main.css
+require('~/main.css')
+
 import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: {
+    content: [
+      "./src/**/*.vue",
+      "./src/**/*.js",
+      "./src/**/*.jsx",
+      "./src/**/*.html",
+      "./src/**/*.pug",
+      "./src/**/*.md",
+    ],
+    safelist: [
+      "body",
+      "html",
+      "img",
+      "a",
+      "ol",
+      "ul",
+      "g-image",
+      "g-image--lazy",
+      "g-image--loaded",
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+  ],
+}
